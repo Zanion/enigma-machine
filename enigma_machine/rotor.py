dd7ab31e8af385dfb70d059722f0f77b63d73ae1
Dustin Kerns <Dkerns0123@gmail.com>
2019-12-14 04:38:45 +0000

    Add wiring to Enigma rotor

--- enigma_machine/rotor.py
+++ enigma_machine/rotor.py
@@ -1,4 +1,7 @@
+from collections import namedtuple
 from enigma_machine.conf.defaults import ALPHABET
+
+Wire = namedtuple("Wire", "contact_in contact_out")
 
 
 class Rotor:
@@ -28,6 +31,42 @@ class Rotor:
     def window(self, window):
         self._window = window.upper()
         self.offset = ALPHABET.index(self.window)
+
+
+    def get_wiring(self, forward=True):
+        """ Get sorted list of wire contacts based on direction of encoding
+
+        Returns a sorted list of wires based on direction of encoding based on
+        index mapping from ALPHABET onto the rotor wiring.
+
+        Args:
+            forward (bool): Direction of encoding (Default: True)
+
+        Returns:
+            Sorted list of wires indexed by mapping to ALPHABET
+
+        """
+        if forward:
+            return sorted(self._wiring, key=lambda x: x.contact_in)
+        return sorted(self._wiring, key=lambda x: x.contact_out)
+
+
+    @property.setter
+    def wiring(self, contact_mapping):
+        """ Set the wiring from a string mapping of ALPHABET onto inbound contacts
+
+        Iterates over the inbound contact_mapping of ALPHABET onto the inbound
+        contacts on the rotor wiring. Populates a wiring array with all contacts
+        such that they can be encoded during forward or reversed passes on the
+        rotor.
+
+        Args:
+            contact_mapping (string): String containing ordered mapping of contacts
+
+        """
+        self._wiring = []
+        for idx in range(len(ALPHABET)):
+            self._wiring.append(Wire(ALPHABET[idx], contact_mapping[idx]))
 
 
     def step(self):
