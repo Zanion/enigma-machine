b51a5bd3fd9f6b8e869433ce75b8a92756c0299e
Dustin Kerns <Dkerns0123@gmail.com>
2019-12-20 04:49:00 +0000

    Initialize plugboard

---
+++ src/enigma_machine/plugboard.py
@@ -0,0 +1,12 @@
+from enigma_machine.wire import Wire
+
+
+class PlugBoard:
+
+
+    def __init__(self):
+        raise NotImplementedError
+
+
+    def swap(letter1, letter2):
+        raise NotImplementedError
