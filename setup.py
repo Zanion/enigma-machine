3998884e28e5128801f796d1ea28563302768372
Dustin Kerns <Dkerns0123@gmail.com>
2019-12-20 04:38:44 +0000

    Refactor to use src directory

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ setup(
     name='enigma-machine',
     version='0.0.1',
     py_modules=['enigma_machine'],
-    packages=find_packages(exclude=['test']),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src', exclude=['test']),
     include_package_data=True,
     install_requires=[
         'click',
     ],
-    entry_points='''
-        [console_scripts]
-        enigma-machine=enigma_machine.__main__:cli
-    ''',
+    entry_points={
+        'console_scripts': {
+            'enigma-machine=__main__:cli'
+        }
+    },
 )
