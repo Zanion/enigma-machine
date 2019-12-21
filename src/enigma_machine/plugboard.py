d9723342b4b100ed4e0611506338c83ce527a407
Dustin Kerns <Dkerns0123@gmail.com>
2019-12-21 05:55:38 +0000

    Add initial swaps to plugboard

--- src/enigma_machine/plugboard.py
+++ src/enigma_machine/plugboard.py
@@ -1,16 +1,30 @@
 from collections import namedtuple
-
-
-Plug = namedtuple("Plug", "left right")
+from enigma_machine.defaults import ALPHABET
 
 
 class PlugBoard:
 
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, plugs={}, alphabet=ALPHABET):
+        self.plugs = plugs
+        self.alphabet = alphabet
 
 
-    def swap(letter1, letter2):
-        raise NotImplementedError
+    @property
+    def alphabet(self):
+        return self._alphabet
+
+
+    @alphabet.setter
+    def alphabet(self, alphabet):
+        assert isinstance(alphabet, str), "Alphabet must be string"
+        assert len(alphabet) > 0, "Alphabet must be non-zero length"
+
+        self._alphabet = alphabet
+
+
+    def add_plug(letter1, letter2):
+        assert letter1 in self.alphabet
+        self.plugs[letter1] = letter2
+        self.plugs[letter2] = letter1
 
