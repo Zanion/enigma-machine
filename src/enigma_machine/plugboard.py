3cf07894a795da3f9b1d7a59250dd7e3f4069ead
Dustin Kerns <Dkerns0123@gmail.com>
2019-12-22 23:20:38 +0000

    Update plugboard swaps

--- src/enigma_machine/plugboard.py
+++ src/enigma_machine/plugboard.py
@@ -2,12 +2,16 @@ from collections import namedtuple
 from enigma_machine.defaults import ALPHABET
 
 
+MAX_PLUGS = 13
+
+
 class PlugBoard:
 
 
-    def __init__(self, plugs={}, alphabet=ALPHABET):
-        self.plugs = plugs
+    def __init__(self, plugs=None, alphabet=ALPHABET, max_plugs=MAX_PLUGS):
         self.alphabet = alphabet
+        self.max_plugs = max_plugs
+        self.configure_plugs(plugs, True)
 
 
     @property
@@ -23,8 +27,42 @@ class PlugBoard:
         self._alphabet = alphabet
 
 
-    def add_plug(letter1, letter2):
-        assert letter1 in self.alphabet
+    def clear_plugs(self):
+        """ Convenience method to clear plugboard """
+        self.configure_plugs(plugs=None, replace=True)
+
+
+    def configure_plugs(self, plugs=None, replace=False):
+        """
+        """
+        assert isinstance(plugs, list) or plugs is None, "Plugs must be list of replacements"
+        assert isinstance(replace, bool), "Replace flag must be boolean"
+
+        if replace:
+            self.plugs = {}
+
+        for plug in plugs if plugs not None:
+            self.add_plug(plug)
+
+
+    def add_plug(self, plug=None):
+        """
+        """
+        assert isinstance(plug, tuple) or plug is None, "Plug must be a tuple"
+
+        if plug is None:
+            return
+
+        # Unpack plug tuple and validate
+        letter1, letter2 = plug
+        assert letter1 in self.alphabet, "Cannot add plugs to letters not in alphabet"
+        assert letter2 in self.alphabet, "Cannot add plugs to letters not in alphabet"
+        assert len(letter1) in self.alphabet, "Plugs must be between single letter character"
+        assert len(letter2) in self.alphabet, "Plugs must be between single letter characters"
+
+        # Assert length < 13
+        assert len(self.plugs) < self.max_plugs, "Cannot add more than 13 plugs to plugboard"
+
         self.plugs[letter1] = letter2
         self.plugs[letter2] = letter1
 
